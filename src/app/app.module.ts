846592adeb2fcfde95f810e078d360c30af1c96a
xnjanx02 <anjana.jk009@gmail.com>
2022-07-08 12:30:12 +0530

    new cpmponent created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewshopComponent } from './viewshop/viewshop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewshopComponent
   ],
   imports: [
     BrowserModule,
